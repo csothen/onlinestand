906e91bc66c8389add669cdfec36185fcc8bf3fa
csothen <cesarjcpinheiro@gmail.com>
2021-02-09 22:43:45 +0000

    Definition of vehicle models

--- pkg/models/vehicle.go
+++ pkg/models/vehicle.go
@@ -1,33 +1,42 @@
 package models
 
-// Vehicle holds all of a vehicle's information
+// Vehicle : Model defining a vehicle
 type Vehicle struct {
-	VIN                   string
-	LicensePlate          string
-	FirstLicensePlateDate string
-	Brand                 string
-	Model                 string
-	Kilometers            int
-	Price                 int
-	Negotiable            bool
-	Sold                  bool
-	Condition             string
-	Extras                []string
-	FuelType              string
-	Gearbox               string
-	CubicCapacity         int
-	EnginePower           int
-	EngineEmissions       int
-	Seats                 int
-	Doors                 int
-	VehicleType           string
-	VehicleColor          string
-	SeatMaterial          string
-	InteriorColor         string
-	WheelSize             int
-	WheelType             string
-	ConstructionYear      string
-	CreatedOn             string
-	UpdatedOn             string
-	DeletedOn             string
+	ID                    int      `json:"id"`
+	LicensePlate          string   `json:"licensePlate"`
+	FirstLicensePlateDate string   `json:"firstLicensePlateDate"`
+	Brand                 string   `json:"brand"`
+	Model                 string   `json:"model"`
+	Kilometers            int      `json:"kilometers"`
+	Price                 int      `json:"price"`
+	Status                string   `json:"status"`
+	Negotiable            bool     `json:"negotiable"`
+	Condition             string   `json:"condition"`
+	Features              []string `json:"features"`
+	FuelType              string   `json:"fuelType"`
+	Gearbox               string   `json:"gearbox"`
+	CubicCapacity         int      `json:"cubicCapacity"`
+	EnginePower           int      `json:"enginePower"`
+	Seats                 int      `json:"seats"`
+	Doors                 int      `json:"doors"`
+	Category              string   `json:"category"`
+	ExteriorColor         string   `json:"exteriorColor"`
+	SeatMaterial          string   `json:"seatMaterial"`
+	InteriorColor         string   `json:"interiorColor"`
+	ConstructionYear      string   `json:"constructionYear"`
+}
+
+// VehicleService : interface for Vehicle model that defines the operations on it
+type VehicleService interface {
+	// Create a vehicle
+	CreateVehicle() error
+
+	// Get a single vehicle based on an id
+	GetVehicle(id int) (*Vehicle, error)
+
+	// Get all vehicles
+	GetAllVehicle() ([]*Vehicle, error)
+
+	// Get all available vehicles
+	GetAllAvailableVehicle() ([]*Vehicle, error)
 }
